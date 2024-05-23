dc25e668b77fa8814767b1e388f2aaa6044ebdfc
Maxwel de Souza Freitas <maxwel@maxwelfreitas.com.br>
2024-05-23 10:31:30 -0300

    Script to run web search

--- wordcloud/run_schsearch.py
+++ wordcloud/run_schsearch.py
@@ -1,16 +1,31 @@
+import argparse
+
 from schsearch import load_sch, SCHWebSearch
 
 if __name__ == '__main__':
+    
+    parser = argparse.ArgumentParser(description='Search Certified Products')    
+    parser.add_argument('total_itens_to_query', type=int, default=10, help='Quantidade de itens para pesquisar')    
+    parser.add_argument('--verbose', type=bool, default=False, help='Imprimir os itens pesquisados')
+    args = parser.parse_args()
+    
+    total_itens_to_query = args.total_itens_to_query
+    verbose = args.verbose
+    
     df_sch = load_sch('datasets/produtos_certificados.zip',grace_period=180)
     df_sch = df_sch[df_sch['Última Pesquisa']==-1]
-    items_to_search = df_sch['Número de Homologação'].head(10).to_list()
+    items_to_search = df_sch['Número de Homologação'].head(total_itens_to_query).to_list()
     
     sch = SCHWebSearch()
     
-    for item in items_to_search:
-        response_code, file_name = sch.google_search(item)
-        print(response_code, file_name.name)
-        
+    for i,item in enumerate(items_to_search):
+        try:
+            response_code, file_name = sch.google_search(item)
+            if verbose:
+                print(i, response_code, file_name.name)
+        except:
+            continue
+            
     print('Pressione ENTER para sair...')
     _ = input()
-        
+    
