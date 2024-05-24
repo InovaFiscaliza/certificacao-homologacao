ea209d24a5ca87b6dc0258022fdffa905760b1e1
Maxwel de Souza Freitas <maxwel@maxwelfreitas.com.br>
2024-05-24 10:07:53 -0300

    Add tokenizer and result file parser

--- schwebsearch/schwebsearch.py
+++ schwebsearch/schwebsearch.py
@@ -1,4 +1,4 @@
-# Author    : Maxwel de Souza Freitas
+#%% Author    : Maxwel de Souza Freitas
 import configparser
 import json
 import os
@@ -12,29 +12,21 @@ import uuid
 from collections import Counter
 from datetime import datetime, timedelta
 from nltk.corpus import stopwords
-from nltk.tokenize import word_tokenize
 from pathlib import Path
 from unicodedata import normalize
 from urllib.parse import urlparse
 
-# CONSTANTS
+#%% CONSTANTS
 
 # time format for json result files
 RESULT_TS_FORMAT = '%Y%m%d%H%M%S%f'
 # time format for wordcloud files
-ANNOTATITION_TS_FORMAT = '%d/%m/%Y %H:%M:%S'
+ANNOTATION_TS_FORMAT = '%d/%m/%Y %H:%M:%S'
+ANOTATION_FILE_TS_FORMAT = '%Y.%m.%d_T%H.%M.%S'
 
-#output folders
-SEARCH_RESULTS_FOLDER = 'datasets/searchresults'
-ANNOTATITIONS_FOLDER = 'datasets/annotations'
-
-# sites to ignore results
-BLACK_LISTED_SITES = ['gov.br', 'fccid.io', 'wer-hat-angerufen.info']
-
-
- # SCH DATABASE
+ #%% SCH DATABASE
  
-def load_sch(file_sch,results_folder=SEARCH_RESULTS_FOLDER,grace_period=0):
+def load_sch(sch_database_file,search_results_folder=None,grace_period=0):
     
     # load SCH database
     usecols = [0,1,11,12,13,14,15]
@@ -42,39 +34,53 @@ def load_sch(file_sch,results_folder=SEARCH_RESULTS_FOLDER,grace_period=0):
     parse_dates = [0]
     date_format = '%d/%m/%Y'
     
-    df_sch = pd.read_csv(file_sch,sep=';',usecols=usecols,dtype=dtype,parse_dates=parse_dates,date_format=date_format)
+    df_sch = pd.read_csv(
+        sch_database_file,
+        sep=';',
+        usecols=usecols,
+        dtype=dtype,
+        parse_dates=parse_dates,
+        date_format=date_format
+        )
+    
     df_sch = df_sch[df_sch['Categoria do Produto']==2]
     df_sch = df_sch.sort_values(by='Data da Homologação',ascending=False)
     df_sch = df_sch.drop_duplicates(subset='Número de Homologação')
-
-    # load search history
-    results_files = list(Path(results_folder).glob('*.json'))
-    search_history = []
-    for file in results_files:
-        search_date, search_engine, search_term, _ = re.split('[_.]',file.name)
-        search_date = datetime.strptime(search_date,RESULT_TS_FORMAT).date()
-        search_history.append([search_term,search_date])
-    columns = ['Número de Homologação', 'Última Pesquisa']
-    df_search_history = pd.DataFrame(search_history,columns=columns)
-    df_search_history = df_search_history.sort_values(by=columns,ascending=[True,False])
-    df_search_history = df_search_history.drop_duplicates(subset='Número de Homologação')
-
-    # merge both dataframes
-    df_sch = df_sch.merge(df_search_history,how='left').fillna(-1)
-
+    
     # filter products certifieds before grace period
     if grace_period > 0:
         certification_date_limit = datetime.today().date() - timedelta(days=grace_period)
         certification_date_limit = certification_date_limit.strftime('%Y-%m-%d')
         df_sch = df_sch[df_sch['Data da Homologação']<=certification_date_limit]
 
+    # load search history
+    if search_results_folder is not None:
+        results_files = list(Path(search_results_folder).rglob('*.json'))
+        search_history = []
+        for file in results_files:
+            search_date, _ , search_term, _ = re.split('[_.]',file.name)
+            search_date = datetime.strptime(search_date,RESULT_TS_FORMAT).date()
+            search_history.append([search_term,search_date])
+            
+        columns = ['Número de Homologação', 'Última Pesquisa']
+        df_search_history = pd.DataFrame(search_history,columns=columns)
+        df_search_history = df_search_history.sort_values(by=columns,ascending=[True,False])
+        df_search_history = df_search_history.drop_duplicates(subset='Número de Homologação')
+
+        # merge both dataframes
+        df_sch = df_sch.merge(df_search_history,how='left').fillna(-1)
+    else:
+        df_sch['Última Pesquisa'] = -1
+          
+    df_sch = df_sch.reset_index(drop=True)
+    
     return df_sch 
 
-# CREDENTIALS
+#%% SEARCH CLASS
 
 class SCHWebSearch(object):
     
-    def __init__(self, search_results_folder=SEARCH_RESULTS_FOLDER, creds_file=None):
+    def __init__(self, search_results_folder, creds_file=None):
         
         if creds_file == None:
             creds_file = Path(os.environ['USERPROFILE'],'creds.ini')
@@ -161,5 +167,151 @@ class SCHWebSearch(object):
         
         return response.status_code, result_file
 
+#%% ANNOTATION
 
+def tokenizer(doc,normalize_words=False):
+
+    stop_words = stopwords.words('portuguese')
+    stop_words.extend(stopwords.words('english'))
+    stop_words.extend(list(string.punctuation))
+
+    # stopwords específicas do domínio
+    stop_words.extend(['cm', 'feature', 'features', 'informações', 'itens', 'leve', 'list', 'nulo', 'package', 
+                       'pacote', 'pacotes', 'recurso', 'tamanho', 'ver', 'anatel', 'laranja', '.', '...',
+                       'complementares', 'peça'])
+
+    if normalize_words:
+        doc = doc.lower()
+        doc = normalize('NFKD', doc).encode('ASCII', 'ignore').decode('ASCII')
+
+    # CountVectorizer token pattern
+    pattern = r'\b\w\w+\b'
+    tokens = [token for token in re.findall(pattern,doc) if token.lower() not in stop_words]
+    # remove number-only tokens
+    tokens = [token for token in tokens if not re.match('\d+',token)]
+
+    return tokens
+
+
+def is_black_listed_site(url,blacklisted_sites=None):
+        if blacklisted_sites is not None:
+            for site in blacklisted_sites:
+                if site in url:
+                    return True
+        return False
+    
+    
+def parse_result_file(file, max_words=25):
+
+    parse_url = lambda url: '.'.join(urlparse(url).netloc.split('.')[-3:])
+
+    EMPTY_RESULT = {'ID': None, 
+                    'DataHora': None,
+                    'Computador': None,
+                    'Usuário': None, 
+                    'Homologação': None, 
+                    'Atributo': 'WordCloud',
+                    'Valor': '',  
+                    'Situação': -1,
+                    'Buscadora': None}
+
+    if isinstance(file, str):
+        file = Path(file)
+    
+    search_date, search_engine, search_term, _ = re.split('[_.]',file.name)
+    search_site = None
+    
+    try:
+        with open(file) as f:
+            results = json.load(f)
+    except:
+        return EMPTY_RESULT
+
+    try:
+        with open('blacklisted_sites.txt') as f:
+            blacklisted_sites = f.readlines()
+            blacklisted_sites = [site.strip() for site in blacklisted_sites]
+    except:
+        blacklisted_sites = None
+        
+    lines = []
+    if search_engine == 'GOOGLE':
+        # results without items in keys are empty
+        if 'items' in results.keys():
+            for item in results['items']:
+                search_site = item['displayLink']
+                if is_black_listed_site(search_site):
+                    continue
+                else:
+                    search_site = parse_url(search_site)
+                    lines.append(item['title'])
+                    if 'snippet' in item.keys():
+                        lines.append(item['snippet'])
+                        
+    elif search_engine == 'BING':
+        # results without webPages in keys are empty
+        if 'webPages' in results.keys():
+            for item in results['webPages']['value'][:10]:
+                search_site = item['url']
+                if is_black_listed_site(search_site):
+                    continue
+                else:
+                    search_site = parse_url(search_site)
+                    lines.append(item['name'])
+                    if 'snippet' in item.keys():
+                        lines.append(item['snippet'])
+                 
+    if len(lines) >= 1:
+        words = tokenizer(' '.join(lines))
+        words_conter = Counter(words)
+        wordCloud_dict = {key:value for key,value in words_conter.most_common(max_words)}
+        wordCloud_json = json.dumps(wordCloud_dict, ensure_ascii=False)
+        situacao = 1
+    else:
+        wordCloud_json = ''
+        situacao = -1
+        
+    wourdCloudInfo_dict = {
+        'metaData': {
+            'Version': 1,
+            'Source': search_engine,
+            'Mode': 'API',
+            'Fields': ['Name', 'Snippet'],
+            'nWords': max_words
+        },
+        'searchedWord': search_term,
+        'cloudOfWords': wordCloud_json
+        }
+    
+    wourdCloudInfo_json = json.dumps(wourdCloudInfo_dict, ensure_ascii=False)
+    
+    return {'ID': str(uuid.uuid4()),
+            'DataHora': datetime.strptime(search_date,RESULT_TS_FORMAT).strftime(ANNOTATION_TS_FORMAT),
+            'Computador': os.environ['COMPUTERNAME'],
+            'Usuário': os.environ['USERNAME'], 
+            'Homologação': f'{search_term[:5]}-{search_term[5:7]}-{search_term[-5:]}', 
+            'Atributo': 'WordCloud',
+            'Valor': wourdCloudInfo_json,  
+            'Situação': situacao,
+            'Buscadora': search_engine}
+    
+def save_annotation_file(df, annotation_folder):
+
+    if isinstance(annotation_folder,str):
+        annotation_folder = Path(annotation_folder)
+
+    df = df.iloc[:,-1]
+
+    annotation_ts = datetime.now().strftime(ANOTATION_FILE_TS_FORMAT)
+    annotation_file = f'Annotation_{annotation_ts}.xlsx'
+    annotation_file = Path(annotation_folder,annotation_file)
+
+    try:
+        df.to_excel(annotation_file,index=False)
+        print(f'Annotation file saved: {annotation_file}')
+    except Exception as ex:
+        print(f'Annotation file not saved: {annotation_file}')
+        raise ex
+
+    return  
 
